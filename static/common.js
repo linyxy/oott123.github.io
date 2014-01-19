b366ca3d71f7c068ddf376ec153d55d6cb93d4e5
oott123 <ip.192.168.1.1@qq.com>
2014-01-19 12:03:09 +0800

    增加IE6支持。

--- static/common.js
+++ static/common.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
 	//菜单切换
-	$('menu').click(function(){
+	$('.menu').click(function(){
 		$('*').stop(true,true);	//停止目前动画
 		var section = $(this).next();	//得到内容section
-		$('section:visible').animate({width:'toggle'},500);	//将目前展开的内容折叠
-		if($('section:visible')[0]!=$(section)[0]){	//如果目前展开的内容不是将要展开的内容
-			$(section).animate({width:'toggle'},1000);	//展开将要展开的内容
+		$('.section:visible').animate({width:'toggle'},500);	//将目前展开的内容折叠
+		if($('.section:visible')[0]!=$(section)[0]){	//如果目前展开的内容不是将要展开的内容
+			$(section).animate({width:'toggle'},1000,'swing',function(){
+				//fix on IE6
+				$(this).css('overflow','auto');
+				$(this).css('overflow-x','hidden');
+			});	//展开将要展开的内容
 		}
 		var color = $.Color(this,'background-color').Darker('50');
 		$('body').animate({backgroundColor:color},800);	//背景颜色渐变
 	});
 	//hint浮现
-	$('hint').mouseover(function(){
+	$('.hint').mouseenter(function(){
 		var offset = $(this).offset();
-		$('tip').css('top',offset.top+20);
-		$('tip').css('left',offset.left+10);
-		$('tip').show();
-		$('tip').html($(this).attr('value'));
+		$('#tip').css('top',offset.top+20);
+		$('#tip').css('left',offset.left+10);
+		$('#tip').show();
+		$('#tip').html($(this).attr('value'));
 	});
 	//hint消失
-	$('hint').mouseout(function(){
-		$('tip').html('');
-		$('tip').hide();
+	$('.hint').mouseleave(function(){
+		$('#tip').html('');
+		$('#tip').hide();
 	});
 	//最喜欢的颜色
-	$('.colorpicker').mouseover(function(){
+	$('.colorpicker').mouseenter(function(){
 		$('*').stop(true,true);	//停止目前动画
 		var oldcolor = $('body').css('background-color');
 		var newcolor = $(this).css('background-color');
@@ -32,7 +36,7 @@ $(document).ready(function(){
 			$('#logo2').fadeIn(500);
 		}
 		$('body').animate({backgroundColor:newcolor},500);
-		$(this).mouseout(function(){
+		$(this).mouseleave(function(){
 			//$('*').stop(false,true);	//停止目前动画
 			//颜色恢复
 			$('body').animate({backgroundColor:oldcolor},500);
